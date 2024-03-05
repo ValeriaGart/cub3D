d21a34748cfe694243f3a5de2e0ab4be9e585f7b
ValeriaGart <valeria20work@gmail.com>
2024-03-05 19:25:32 +0100

    fixed fisheye, added textures, added collision

--- src/data.c
+++ src/data.c
@@ -1,46 +1,51 @@
 #include "../incl/cub3d.h"
 
-void    ft_set_plr(t_map *map, t_data *data)
+void	ft_player_found(t_data *data, t_map *map, int y, int x)
 {
-    int x;
-    int y;
+	data->plr.x = x;
+	data->plr.y = y;
+	data->plr.direction = map->real_map[y][x];
+	if (map->real_map[y][x] == 'N')
+		data->plr.angle = PI / 2;
+	if (map->real_map[y][x] == 'W')
+		data->plr.angle = PI;
+	if (map->real_map[y][x] == 'S')
+		data->plr.angle = 3 * PI / 2;
+}
 
-    x = 0;
-    y = 0;
-    data->plr.angle = 0;
-    while (map->real_map[y])
-    {
-        x = 0;
-        while (map->real_map[y][x])
-        {
-            if (ft_strchr("NWES", map->real_map[y][x]))
-            {
-                data->plr.x = x;
-                data->plr.y = y;
-                data->plr.direction = map->real_map[y][x];
-                if (map->real_map[y][x] == 'N')
-                    data->plr.angle = PI / 2;
-                if (map->real_map[y][x] == 'W')
-                    data->plr.angle = PI;
-                if (map->real_map[y][x] == 'S')
-                    data->plr.angle = 3 * PI / 2;
-                return ;
-            }
-            x++;
-        }
-        y++;
-    }
+void	ft_set_plr(t_map *map, t_data *data)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
 	data->plr.angle = 0;
-    data->plr.delta_x = cos(data->plr.angle) * 5;
+	while (map->real_map[y])
+	{
+		x = 0;
+		while (map->real_map[y][x])
+		{
+			if (ft_strchr("NWES", map->real_map[y][x]))
+			{
+				ft_player_found(data, map, y, x);
+				return ;
+			}
+			x++;
+		}
+		y++;
+	}
+	data->plr.angle = 0;
+	data->plr.delta_x = cos(data->plr.angle) * 5;
 	data->plr.delta_y = sin(data->plr.angle) * 5;
 }
 
-void ft_set_data(t_data *data)
+void	ft_set_data(t_data *data)
 {
-    data->map = NULL;
-    data->mlx_ptr = NULL;
-    data->win_ptr = NULL;
-    data->smallw_enabled = true;
+	data->map = NULL;
+	data->mlx_ptr = NULL;
+	data->win_ptr = NULL;
+	data->smallw_enabled = true;
 	data->raycast.posX = 0;
 	data->raycast.posY = 0;
 }
--- src/renders_extra.c
+++ src/renders_extra.c
@@ -48,10 +48,65 @@ void	ft_put_pixel_raycast(t_data *data, int colour, int z)
 	*((unsigned int *)(offset + (&(data->img))->addr)) = colour;
 }
 
-void	ft_drawtheline(int x, int y, t_data *data, int colour)
+/*void	ft_drawtheline(int x, int y, t_data *data, int colour)
 {
 	while (x < y)
 	{
+		ft_put_pixel_raycast(data, colour, x);
+		x += WIDTH_X;
+	}
+}*/
+
+double	get_step(t_rc *rc)
+{
+	double	wall;
+
+	if (!rc->side)
+		wall = rc->posY + rc->perpWallDist * rc->rayDirY;
+	else
+		wall = rc->posX + rc->perpWallDist * rc->rayDirX;
+	rc->tex = (int)((double)(wall - floor(wall)) * (double)64);
+	if ((!rc->side && rc->rayDirX < 0)
+		|| (rc->side && rc->rayDirY > 0))
+		rc->tex = 64 - rc->tex - 1;
+	return (1.0 * (double)64 / (double)rc->lineheight);
+}
+
+//embrace the chaos version
+/*void	ft_draw_pic(int x, int y, t_data *data, int colour, int real_x)
+{
+	double	step;
+	double	tex_p;
+
+	(void)real_x;
+	step = get_step(&data->raycast);
+	tex_p = (x - HEIGHT_Y / 2 + data->raycast.lineheight / 2) * step;
+	while (x <= y)
+	{
+		data->raycast.tex_y = (int)tex_p & 63;
+		tex_p += step;
+		colour = *(int *)(data->map->nor(comment.addr)
+				+ (data->raycast.tex_y * data->no.line_size
+					+ data->raycast.tex * (data->no.bits_per_pixel / 8)));
+		ft_put_pixel_raycast(data, colour, x);
+		x += WIDTH_X;
+	}
+}*/
+
+void	ft_draw_pic(int x, int y, t_data *data, int colour, int real_x)
+{
+	double	step;
+	double	tex_p;
+
+	step = get_step(&data->raycast);
+	tex_p = (((x - real_x) / WIDTH_X) - HEIGHT_Y / 2 + data->raycast.lineheight / 2) * step;
+	while (x <= y)
+	{
+		data->raycast.tex_y = (int)tex_p & 63;
+		tex_p += step;
+		colour = *(int *)(data->no.addr
+				+ (data->raycast.tex_y * data->no.line_size
+					+ data->raycast.tex * (data->no.bits_per_pixel / 8)));
 		ft_put_pixel_raycast(data, colour, x);
 		x += WIDTH_X;
 	}
